9bd574ade26d8d0a3c7ff328c19236c3f77ea96c
Alexandre Resende <aar6947@gmail.com>
2019-01-10 23:04:41 -0200

    component: Create header component

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import HeaderSC from './header.sc';
+
+const Header = (props) => (
+  <HeaderSC>Poupa-up</HeaderSC>
+);
+
+export default Header;
---
+++ src/components/header/header.sc.js
@@ -0,0 +1,15 @@
+
+import styled from 'styled-components';
+
+const Header = styled.div`
+  background-color: #3D5467;
+  color: white;
+  font-size: 3rem;
+  text-align: center;
+  width: 100%;
+  height: 5rem;
+  position: fixed;
+  top: 0;
+`;
+
+export default Header;
