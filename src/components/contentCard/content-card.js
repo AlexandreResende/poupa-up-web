d11db8066ddb948ee5ce606220b4558eac483658
Alexandre Resende <aar6947@gmail.com>
2019-01-16 23:17:28 -0200

    chore: Fetch data from get-all-* to content-cards component

--- src/components/contentCard/content-card.js
+++ src/components/contentCard/content-card.js
@@ -9,6 +9,26 @@ class ContentCard extends Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    const titleLowered = this.props.title.toLowerCase();
+    const endpoint = this.getEndoint(titleLowered);
+
+    fetch(endpoint, { method: 'GET' })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  getEndoint(title) {
+    return `http://localhost:3000/${title}/get-all-${title}`;
+  }
+
   render() {
     return (
       <ContentCardSC
