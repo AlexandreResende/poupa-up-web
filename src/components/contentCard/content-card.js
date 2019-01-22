e6c8708b916ec60ecab1a1382d88f355339ee31e
AlexandreResende <alexandre.amres@gmail.com>
2019-01-21 22:49:56 -0200

    refactor: Change tables using react-table

--- src/components/contentCard/content-card.js
+++ src/components/contentCard/content-card.js
@@ -1,9 +1,11 @@
 
 import React, { Component } from 'react';
+import ReactTable from 'react-table';
 
 import ContentCardSC from './content-card.sc';
 
-import './content-card.css';
+// import './content-card.css';
+import "react-table/react-table.css";
 
 class ContentCard extends Component {
   constructor(props) {
@@ -43,13 +45,40 @@ class ContentCard extends Component {
 
   render() {
     const titleLowered = this.props.title.toLowerCase();
+    const columns = [
+      { 
+        Header: `${this.props.title}`,
+        columns: [
+          { Header: 'Value', accessor: 'value', maxWidth: 100 },
+          { Header: 'Description', accessor: 'description', maxWidth: 300 }
+        ]
+      }
+    ];
+    const data = this.state[titleLowered].map(data => {
+        const newData = { ...data };
+        newData.value = `R$ ${data.value}`;
+
+        return newData;
+      }
+    );
 
     return (
       <ContentCardSC
         marginRight={this.props.marginRight}
         marginLeft={this.props.marginLeft}
       >
-        <table>
+        <ReactTable
+          className='-striped'
+          data={data}
+          columns={columns}
+          showPagination={false}
+          showPaginationBottom={false}
+          showPageSizeOptions={false}
+          defaultPageSize={8} 
+          sortable={false}
+          noDataText=''
+        />
+        {/* <table>
           <thead><tr><th colSpan='2'>{this.props.title}</th></tr></thead>
           <tbody>
             <tr>
@@ -60,7 +89,7 @@ class ContentCard extends Component {
             return (<tr key={objValue.id}><td>R$ {objValue.value}</td><td>{objValue.description}</td></tr>);
             })}
           </tbody>
-        </table>
+        </table> */}
       </ContentCardSC>
     );
   }
